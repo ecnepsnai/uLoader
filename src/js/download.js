543b68e67b576d2bf203b6d47fb788eab787712f
Ian Spence <ian@ecnepsnai.com>
2016-10-07 20:44:33 -0700

    Some file filtering

--- src/js/download.js
+++ src/js/download.js
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener(function(message) {
-    console.log(message);
     if (message.event === 'uLoader-download-file') {
         chrome.downloads.download({
             url: message.url
--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,20 @@
 $(function() {
+    var supportedTypes = [
+        '.au', '.avi', '.bmp', '.bz2', '.css', '.dtd', '.doc', '.docx', '.dotx', '.es',
+        '.exe', '.gif', '.gz', '.hqx', '.jar', '.jpg', '.js', '.midi', '.mp3',
+        '.mpeg', '.ogg', '.pdf', '.pl', '.png', '.potx', '.ppsx', '.ppt', '.pptx',
+        '.ps', '.qt', '.ra', '.ram', '.rdf', '.rtf', '.sgml', '.sit', '.sldx', '.svg',
+        '.swf', '.tar.gz', '.tgz', '.tiff', '.tsv', '.txt', '.wav', '.webm', '.webp',
+        '.xlam', '.xls', '.xlsb', '.xlsx', '.xltx', '.xml', '.zip'
+    ];
+
     var searchNodes = [
+        {
+            node: 'a',
+            urlFunction: function(node) {
+                return $(node).attr('href');
+            }
+        },
         {
             node: 'img',
             urlFunction: function(node) {
@@ -27,15 +42,32 @@ $(function() {
         return link;
     };
 
+    var linkFilter = function(link) {
+        var valid = false;
+        supportedTypes.forEach(function(type) {
+            if (link.endsWith(type)) {
+                valid = true;
+            }
+        });
+        return valid;
+    };
+
+    var typeFormatter = function(link) {
+        var components = link.split('.');
+        if (components.length > 0 && link.indexOf('.') !== -1) {
+            var type = components[components.length - 1];
+            return type;
+        }
+    };
+
     var $nodes, link, components, type;
     searchNodes.forEach(function(searchNode) {
         $nodes = $('body').find(searchNode.node);
         $nodes.each(function(idx, node) {
             link = searchNode.urlFunction(node);
-            if (link) {
+            if (link && linkFilter(link)) {
                 link = linkFormatter(link);
-                components = link.split('.');
-                type = components[components.length - 1];
+                type = typeFormatter(link);
                 files.push({
                     url: link,
                     type: type
