799b4e4bcbf2eed9bdaf4c0de772545467cc8975
Ian Spence <ian@ecn.io>
2018-04-01 14:47:45 -0700

    Improved filtering & bug fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 $(function() {
     var injectedFiles = [
-        'jquery-3.1.1.min.js',
         'search.js'
     ];
 
@@ -10,7 +9,50 @@ $(function() {
         });
     });
 
+    class Filter {
+        constructor(matching, types) {
+            this.matching = matching;
+            this.types = types;
+        }
+
+        matches(file) {
+            var matched = false;
+
+            if (this.matching !== undefined) {
+                if (file.url.match(this.matching) !== null) {
+                    matched = true;
+                } else {
+                    matched = false;
+                }
+            }
+            if (!this.noTypes()) {
+                if (currentFilter.types[file.type]) {
+                    matched = true;
+                } else {
+                    matched = false;
+                }
+            }
+
+            return matched;
+        }
+
+        noTypes() {
+            if (this.types === {}) {
+                return true;
+            }
+            var allTypes = Object.values(this.types);
+            for (var i = 0; i < allTypes.length; i++) {
+                if (allTypes[i] !== false) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
     var files = [],
+        currentFilter = new Filter(undefined, {}),
         $filters = $('#filters'),
         $files = $('#files'),
         $downloadButton = $('#download'),
@@ -35,22 +77,14 @@ $(function() {
         if (text.length > 0) {
             try {
                 var expression = new RegExp(text, 'g');
-                files.forEach(function(file, idx) {
-                    if (file.url.match(expression) !== null) {
-                        file.download = true;
-                    } else {
-                        file.download = false;
-                    }
-                    $('#file-' + idx).prop('checked', file.download);
-                });
+                currentFilter.matching = expression;
+                filterFiles();
             } catch (e) {
                 // Don't worry about it
             }
         } else {
-            files.forEach(function(file, idx) {
-                file.download = false;
-                $('#file-' + idx).prop('checked', file.download);
-            });
+            currentFilter.matching = undefined;
+            filterFiles();
         }
     }));
 
@@ -58,21 +92,50 @@ $(function() {
         var $checkbox = $('<input type="checkbox" />'),
             $label = $('<label></label>');
         $checkbox.on('click', function() {
-            files.forEach(function(file, idx) {
-                if (file.type === filter) {
-                    file.download = true;
-                    $('#file-' + idx).click();
-                }
-            });
+            currentFilter.types[filter] = this.checked;
+            filterFiles();
         });
         $label.append($checkbox).append($('<span>' + filter + '</span>'));
         $filters.append($label);
     };
 
+    var filterFiles = function() {
+        var matchedCount = 0;
+        files.forEach(function(file, idx) {
+            if (currentFilter.matches(file)) {
+                matchedCount ++;
+                file.download = true;
+                $('#file-' + idx).prop('checked', file.download);
+            } else {
+                file.download = false;
+                $('#file-' + idx).prop('checked', file.download);
+            }
+        });
+        updateButton();
+    };
+
+    var updateButton = function() {
+        var numFilesToDownload = 0;
+        files.forEach(function(file) {
+            if (file.download) {
+                numFilesToDownload ++;
+            }
+        });
+
+        if (numFilesToDownload > 0) {
+            $downloadButton.attr('disabled', false);
+            $downloadButton.text('Download Files (' + numFilesToDownload + ')');
+        } else {
+            $downloadButton.attr('disabled', true);
+            $downloadButton.text('Download Files');
+        }
+    }
+
     var addFileCheckbox = function(file, idx) {
         var $checkbox = $('<input type="checkbox" id="file-' + idx + '"/>');
         $checkbox.on('click', function() {
-            file.download = true;
+            file.download = this.checked;
+            updateButton();
         });
         return $checkbox;
     };
@@ -96,10 +159,6 @@ $(function() {
             files.forEach(function(file, idx) {
                 addFile(file, idx);
             });
-
-            if (files.length > 0) {
-                $downloadButton.prop('disabled', false);
-            }
         }
     });
 });
